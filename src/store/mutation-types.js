d23dfb032c77ff4f76fb03172cc884e08e63b66d
gaoguoping19 <129843930+gaoguoping19@users.noreply.github.com>
2023-04-11 11:06:18 +0800

    Update mutation-types.js

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,7 +1,7 @@
 //AI头像地址设置
 export const AI_HEAD_IMG_URL="https://th.bing.com/th?id=ODL.3e2fbff4543f0d3632d34be6d02adc93&w=100&h=100&c=12&pcl=faf9f7&o=6&dpr=1.5&pid=13.1"
 //用户头像地址设置
-export const USER_HEAD_IMG_URL="https://github.com/gaoguoping19/ChatGPT_JCM/blob/main/src/mystyle.png"
+export const USER_HEAD_IMG_URL="https://i.postimg.cc/D0by10Kf/mystyle.png"
 //用户名称设置
 export const USER_NAME="夏思出"
 
